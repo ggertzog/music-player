69d20d23612b57cda9b7d213c4988dda1a2b35ca
ggertzog <musienko.roma@mail.ru>
2024-08-08 15:05:56 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  base: '/', 
-  build: {
-    outDir: 'build'
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/music-player/' : '/',
+    build: {
+      outDir: 'build'
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 });
